f469aebed219412f548d83d66b554d84e842c995
victor.silva <victor.silva@CONTW-DES21.CONTMATIC.AD>
2017-05-15 15:20:15 -0300

    comita ai

--- Lista/src/br/com/contimatic/lista/ListaNum2.java
+++ Lista/src/br/com/contimatic/lista/ListaNum2.java
@@ -61,6 +61,58 @@ public class ListaNum2 {
 		}
 	}
 
+	public No removerNoComeco() {
+		if (isEmpty()) {
+			System.out.println("Lista vazia");
+			return null;
+		} else {
+			No aux = inicio;
+			inicio = (No) inicio.getNext();
+			numElementos--;
+			return aux;
+		}
+	}
+
+	public No removerNoFim() {
+		No aux = inicio;
+		if (isEmpty()) {
+			System.out.println("Lista vazia");
+			return null;
+		} else {
+			for (int i = 0; i < numElementos - 2; i++) {
+				aux = (No) aux.getNext();
+			}
+			aux.setNext(null);
+			fim = aux;
+			numElementos--;
+			return aux;
+		}
+	}
+
+	public No removerEmQulaquerPosicao(int posicao) {
+		if (isEmpty() || posicao < numElementos && posicao > numElementos) {
+			System.out.println("Lista vazia ou posição invalida");
+			return null;
+		} else if (posicao == 1) {
+			removerNoComeco();
+		} else if (posicao == numElementos) {
+			removerNoFim();
+		} else {
+			No anterior = inicio;
+			No atual = (No) inicio.getNext();
+			for (int i = 0; i < posicao - 2; i++) {
+				anterior = (No) anterior.getNext();
+				atual = (No) atual.getNext();
+			}
+
+			anterior.setNext(atual.getNext());
+			atual.setNext(null);
+			numElementos--;
+			return atual;
+		}
+		return null;
+	}
+
 	public String toString() {
 
 		// Verificando se a Lista está vazia
